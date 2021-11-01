c2ea22efbf62b30073a70d2e8e0f3d24765070c6
Cory Holenstein <cholensteinwork@gmail.com>
2021-11-01 12:47:50 -0400

    slideshow now works, needs some more stlying

--- src/GlobalWebpageHooks/Slideshow.js
+++ src/GlobalWebpageHooks/Slideshow.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import dance_image_one from '../Photos/dance_1.jpg';
+import dance_image_two from '../Photos/dance_2.jpg';
+import dance_image_three from '../Photos/dance_3.jpg';
+import dance_image_four from '../Photos/dance_4.jpg';
 
 const slideImages = [
     {
-        url: '',
+        url: dance_image_one,
         caption: 'Slide 1'
     },
     {
-        url: './../Photos/dance_2.jpg',
+        url: dance_image_two,
         caption: 'Slide 2'
     },
     {
-        url: './Photos/dance_3.jpg',
+        url: dance_image_three,
         caption: 'Slide 3'
     },
     {
-        url: '../Photos/dance_4.jpg',
+        url: dance_image_four,
         caption: 'Slide 4'
     },
 ];
@@ -28,7 +31,8 @@ const Slideshow = () => {
             <Slide>
                 {slideImages.map((slideImage, index) => (
                     <div className="each-slide" key={index}>
-                        <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+                        <div>
+                            <img src={slideImage.url} width="25%" height="25%" /><br></br>
                             <span>{slideImage.caption}</span>
                         </div>
                     </div>
