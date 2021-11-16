5b254de2cba6c95baed82471e66ecdccbc1b6c69
Cory Holenstein <cholensteinwork@gmail.com>
2021-11-16 13:40:53 -0500

    about page and connect page margins updated

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -26,6 +26,7 @@ const About = () => {
                 <h1>About Us!</h1>
                 <p>MorningStar dance is an award winning dance studio located in johns creek. </p>
                 <p>Founded in 2004, MorningStar Dance Academy of Atlanta (MSDA) offers high quality training in classical ballet, pointe, contemporary dance, jazz, character dance, and Chinese traditional folk dance.
+                    </p> <p>
                     The school's teaching methods offer students an age appropriate, challenging, and enjoyable
                     program that combines personal enrichment with technical preparation for professional work.</p>
 
@@ -49,7 +50,7 @@ const About = () => {
                     Students of MSDA are offered many performance opportunities throughout the year. They participate frequently in local community activities such as multi-cultural festivals and celebrations, and are also provided many opportunities to perform whole act ballets such as Coppelia, Sleeping Beauty, Swan Lake, etc. This allows them to gain valuable stage experiences as well as confidence to perform in the future.
                     </p> <p>
                     In 2014, MorningStar Dance Academy received the "Most Outstanding School" award at the YAGP regional semi-final competition for its consistent performance and dedication to dance education. Over the years, students from MSDA received several first places in the classical ballet, contemporary and ensemble categories in YAGP semi-finals and also
-
+                    </p> <p>
                     qualified many times for the international finals in New York. Students also won many high awards at other national and international dance competitions such as Showstopper, International Dance Challenge (IDC), American Dance Championships (ADC), World Ballet Competition (WBC), and American Ballet Competition (ABC). Due to their achievements, several students were awarded full scholarships to top professional ballet schools such as Joffrey Ballet School,
                     The Rock School of Dance Education, Princess Grace Academy of Classical Dance, etc. Furthermore, a graduate of MSDA was accepted with a scholarship this year to study in dance at the renowned Tisch School of the Arts of New York University.
                     Most recently, a current student was the prizewinner of a distinguished professional ballet competition, Beijing International Ballet and Choreography competition (IBCC) on July 2015.
